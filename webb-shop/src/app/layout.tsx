759043fcc056a55861c476626aea7baeebf4a185
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-07-16 16:58:25 +0200

    installed next.js application

---
+++ webb-shop/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Webb Shop App",
+  description: "Webb Shop App revolutionizes your shopping experience with a wide selection of 20 products.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
