a7369566de206d30e09a271a90fe5384b03449a1
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-07-16 18:54:43 +0200

    installed mongoose

---
+++ src/app/lib/mongodb.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const cached: {
+  connection?: typeof mongoose;
+  promise?: Promise<typeof mongoose>;
+} = {};
+
+async function connectMongo() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    );
+  }
+  if (cached.connection) {
+    console.log('Using cached MongoDB connection');
+    return cached.connection;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log('MongoDB connected');
+      return mongoose;
+    });
+  }
+  try {
+    cached.connection = await cached.promise;
+  } catch (e) {
+    cached.promise = undefined;
+    throw e;
+  }
+  return cached.connection;
+}
+export default connectMongo;
