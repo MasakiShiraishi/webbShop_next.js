c8f5329f489db49c867ebab0658384f44961c805
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-07-19 13:15:08 +0200

    created mens-clothing page

---
+++ src/app/(pages)/products/mens-clothing/page.tsx
@@ -0,0 +1,7 @@
+import ProductsList from "@/app/ui/products";
+
+export default function MensClothing() {
+          return (
+                    <ProductsList category="men's clothing" />
+          );
+}
