d5c719f6cdbd6414402101af3e1364b2adadeece
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-07-19 13:19:16 +0200

    created womens-clothing page

---
+++ src/app/(pages)/products/womens-clothing/page.tsx
@@ -0,0 +1,7 @@
+import ProductsList from "@/app/ui/products";
+
+export default function WomensClothing() {
+          return (
+                    <ProductsList category="women's clothing" />
+          );
+}
