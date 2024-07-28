2238eb7eb6193e1fe70dcde12e019fc7bc451e1b
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-07-28 08:58:08 +0200

    created 2 files because of fetching orders from databas

---
+++ src/app/(pages)/api/fetchOrders.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import connectMongo from '../../lib/mongodb';
+import OrderModel from '@/app/lib/orders';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('API Endpoint Hit');
+  await connectMongo(); 
+  console.log('MongoDB connected');
+
+  try {
+    const customersOrder = await OrderModel.find({});
+    console.log('Transactions fetched:', customersOrder);
+    res.status(200).json({ customersOrder });
+  } catch (e) {
+    console.error('Error fetching customersOrder:', e);
+    res.status(500).json({ error: 'Failed to fetch customersOrder' });
+  }
+}
