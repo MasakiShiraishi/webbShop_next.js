7cc86ac6e62701e2659392f62ecbba8896fc0643
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-08-01 14:56:06 +0200

    created logout function

--- src/app/ui/customersOrders.tsx
+++ src/app/ui/customersOrders.tsx
@@ -1,7 +1,9 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import { Order, Product } from '../lib/definitions';
-
+import LogOut from './logOut';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '../lib/auth';
 
 
 export default function CustomersOrdersList() {
@@ -11,12 +13,26 @@ export default function CustomersOrdersList() {
   const [products, setProducts] = useState<Product[]>([]);
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [loading, setLoading] = useState(true); // Add loading state
+  const router = useRouter();
+
+  useAuth();
 
   useEffect(() => {
+    // Check authentication on page load
+    const token = sessionStorage.getItem('auth');
+    if (!token) {
+      // Redirect to login page if not authenticated
+      router.push('/login');
+      return;
+    }
     const fetchCustomersOrders = async () => {
       try {
         console.log('Fetching customersOrder...');
-        const res = await fetch('/api/fetchOrders');
+        const res = await fetch('/api/fetchOrders', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         if (!res.ok) {
           throw new Error('Failed to fetch customersOrder');
         }
@@ -35,7 +51,7 @@ export default function CustomersOrdersList() {
     };
 
     fetchCustomersOrders();
-  }, []);
+  }, [router]);
   // check for display size
   useEffect(() => {
     const handleResize = () => {
@@ -179,195 +195,199 @@ export default function CustomersOrdersList() {
     } catch (error) {
       console.error('Error updating order:', error);
     }
-  }; 
+  };
 
   return (
     <div>
-      <h2 className="text-3xl text-center font-bold m-6">Order's list</h2>
+      <div className="flex justify-between m-6">
+        <h2 className="text-3xl text-center font-bold ">Order's list</h2>
+        {/* added log out button */}
+        <LogOut />
+      </div>
       {loading ? ( // Show a loading indicator while data is being fetched
         <p className="text-3xl text-center font-bold m-6">Loading...</p>
       ) :
-      customersOrder.length > 0 ? (
-        <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4">
-          {customersOrder.map((order, index) => (
-            <li
-              key={index}
-              className="list-none border border-black hover:border-cyan-500 hover:border-4 rounded-lg m-4"
-            >
-              {editIndex === index ? (
-                <>
-                  <div className="p-2 ml-6">
-                    <div className="text-xl font-bold">
+        customersOrder.length > 0 ? (
+          <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4">
+            {customersOrder.map((order, index) => (
+              <li
+                key={index}
+                className="list-none border border-black hover:border-cyan-500 hover:border-4 rounded-lg m-4"
+              >
+                {editIndex === index ? (
+                  <>
+                    <div className="p-2 ml-6">
+                      <div className="text-xl font-bold">
+                        Items:
+                        <ul>
+                          {order.items.map((item, itemIndex) => (
+                            <li
+                              key={itemIndex}
+                              className="text-lg border border-solid border-current"
+                            >
+                              <label htmlFor={`item-ID-${index}-${itemIndex}`} className="flex">
+                                Item ID:
+                              </label>
+                              <input
+                                className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 mx-6 text-center w-24"
+                                type="number"
+                                id={`item-ID-${index}-${itemIndex}`}
+                                name="item-ID"
+                                placeholder="Input item ID"
+                                min="1"
+                                max="20"
+                                value={item.id}
+                                onChange={(e) =>
+                                  handleEditChange(
+                                    index,
+                                    `item.${itemIndex}.id`,
+                                    e.target.value)
+                                }
+                                required
+                                onInput={(e) => {
+                                  const target = e.target as HTMLInputElement;
+                                  target.value = normalizeInput(target.value);
+                                }}
+                              />
+
+                              <input
+                                className="pr-4"
+                                id={`item-title-${index}-${itemIndex}`}
+                                name="item-title"
+                                type="text"
+                                value={item.title}
+                                disabled
+                              />
+                              <label htmlFor={`item-quantity-${index}-${itemIndex}`} className="flex">
+                                Item Quantity
+                              </label>
+                              <input
+                                className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 mx-6 text-center w-24"
+                                type="number"
+                                id={`item-quantity-${index}-${itemIndex}`}
+                                name="item-quantity"
+                                min="1"
+                                value={item.quantity}
+                                onChange={(e) =>
+                                  handleEditChange(
+                                    index,
+                                    `item.${itemIndex}.quantity`,
+                                    e.target.value
+                                  )
+                                }
+                                onInput={(e) => {
+                                  const target = e.target as HTMLInputElement;
+                                  target.value = normalizeInput(target.value);
+                                }}
+                              />
+                              <input
+                                id={`item-total-${index}-${itemIndex}`}
+                                name="item-total"
+                                type="number"
+                                value={item.total}
+                                disabled
+                              />
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                      <input
+                        className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
+                        type="text"
+                        name="name"
+                        placeholder="Input full name"
+                        value={order.name}
+                        onChange={(e) =>
+                          handleEditChange(index, 'name', e.target.value)
+                        }
+                        required
+                      />
+                      <input
+                        className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
+                        id="address"
+                        name="address"
+                        type="text"
+                        placeholder="Input Address"
+                        value={order.address}
+                        onChange={(e) =>
+                          handleEditChange(index, 'address', e.target.value)
+                        }
+                        required
+                      />
+                      <input
+                        className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
+                        id="email-address"
+                        name="email"
+                        type="email"
+                        placeholder="Input Email"
+                        value={order.email}
+                        onChange={(e) =>
+                          handleEditChange(index, 'email', e.target.value)
+                        }
+                        required
+                      />
+                      <input
+                        className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
+                        id="delivery-option"
+                        name="deliveryOption"
+                        type="number"
+                        min="1"
+                        max="3"
+                        placeholder="Choose your delivery..."
+                        value={order.deliveryOption}
+                        onChange={(e) =>
+                          handleEditChange(index, 'deliveryOption', e.target.value)
+                        }
+                        onInput={(e) => {
+                          const target = e.target as HTMLInputElement;
+                          target.value = normalizeInput(target.value);
+                        }}
+                        required
+                      />
+                    </div>
+                    <button
+                      className="p-2 bg-blue-500 text-white"
+                      onClick={() => saveOrderChanges(index)}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="p-2 bg-red-500 text-white"
+                      onClick={() => setEditIndex(null)}
+                    >
+                      Cancel
+                    </button>
+                    <div className="text-2xl text-red-500 mt-2">{errorMessage}</div>
+
+                  </>
+                ) : (
+                  <>
+                    <div className="text-xl font-bold p-2 ml-2">
                       Items:
                       <ul>
                         {order.items.map((item, itemIndex) => (
-                          <li
-                            key={itemIndex}
-                            className="text-lg border border-solid border-current"
-                          >
-                            <label htmlFor={`item-ID-${index}-${itemIndex}`} className="flex">
-                              Item ID:
-                            </label>
-                            <input
-                              className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 mx-6 text-center w-24"
-                              type="number"
-                              id={`item-ID-${index}-${itemIndex}`}
-                              name="item-ID"
-                              placeholder="Input item ID"
-                              min="1"
-                              max="20"
-                              value={item.id}
-                              onChange={(e) =>
-                                handleEditChange(
-                                  index,
-                                  `item.${itemIndex}.id`,
-                                  e.target.value)
-                              }
-                              required
-                              onInput={(e) => {
-                                const target = e.target as HTMLInputElement;
-                                target.value = normalizeInput(target.value);
-                              }}
-                            />
-
-                            <input
-                              className="pr-4"
-                              id={`item-title-${index}-${itemIndex}`}
-                              name="item-title"
-                              type="text"
-                              value={item.title}
-                              disabled
-                            />
-                            <label htmlFor={`item-quantity-${index}-${itemIndex}`} className="flex">
-                              Item Quantity
-                            </label>
-                            <input
-                              className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 mx-6 text-center w-24"
-                              type="number"
-                              id={`item-quantity-${index}-${itemIndex}`}
-                              name="item-quantity"
-                              min="1"
-                              value={item.quantity}
-                              onChange={(e) =>
-                                handleEditChange(
-                                  index,
-                                  `item.${itemIndex}.quantity`,
-                                  e.target.value
-                                )
-                              }
-                              onInput={(e) => {
-                                const target = e.target as HTMLInputElement;
-                                target.value = normalizeInput(target.value);
-                              }}
-                            />
-                            <input
-                              id={`item-total-${index}-${itemIndex}`}
-                              name="item-total"
-                              type="number"
-                              value={item.total}
-                              disabled
-                            />
+                          <li key={itemIndex} className="text-lg border border-solid border-current">
+                            <p>ID: {item.id}</p>
+                            <p>Title: {item.title}</p>
+                            <p>Quantity: {item.quantity}</p>
+                            <p>Total: {item.total}</p>
                           </li>
                         ))}
                       </ul>
                     </div>
-                    <input
-                      className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
-                      type="text"
-                      name="name"
-                      placeholder="Input full name"
-                      value={order.name}
-                      onChange={(e) =>
-                        handleEditChange(index, 'name', e.target.value)
-                      }
-                      required
-                    />
-                    <input
-                      className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
-                      id="address"
-                      name="address"
-                      type="text"
-                      placeholder="Input Address"
-                      value={order.address}
-                      onChange={(e) =>
-                        handleEditChange(index, 'address', e.target.value)
-                      }
-                      required
-                    />
-                    <input
-                      className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
-                      id="email-address"
-                      name="email"
-                      type="email"
-                      placeholder="Input Email"
-                      value={order.email}
-                      onChange={(e) =>
-                        handleEditChange(index, 'email', e.target.value)
-                      }
-                      required
-                    />
-                    <input
-                      className="border-dashed border-2 border-slate-700 hover:border-double hover:border-4 hover:bg-sky-200 text-xl font-bold"
-                      id="delivery-option"
-                      name="deliveryOption"
-                      type="number"
-                      min="1"
-                      max="3"
-                      placeholder="Choose your delivery..."
-                      value={order.deliveryOption}
-                      onChange={(e) =>
-                        handleEditChange(index, 'deliveryOption', e.target.value)
-                      }
-                      onInput={(e) => {
-                        const target = e.target as HTMLInputElement;
-                        target.value = normalizeInput(target.value);
-                      }}
-                      required
-                    />
-                  </div>
-                  <button
-                    className="p-2 bg-blue-500 text-white"
-                    onClick={() => saveOrderChanges(index)}
-                  >
-                    Save
-                  </button>
-                  <button
-                    className="p-2 bg-red-500 text-white"
-                    onClick={() => setEditIndex(null)}
-                  >
-                    Cancel
-                  </button>                  
-                    <div className="text-2xl text-red-500 mt-2">{errorMessage}</div>
-                 
-                </>
-              ) : (
-                <>
-                  <div className="text-xl font-bold p-2 ml-2">
-                    Items:
-                    <ul>
-                      {order.items.map((item, itemIndex) => (
-                        <li key={itemIndex} className="text-lg border border-solid border-current">
-                          <p>ID: {item.id}</p>
-                          <p>Title: {item.title}</p>
-                          <p>Quantity: {item.quantity}</p>
-                          <p>Total: {item.total}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <p className="text-xl font-bold p-2 ml-6">Name: {order.name}</p>
-                  <p className="text-xl font-bold p-2 ml-6">Address: {order.address}</p>
-                  <p className="text-xl font-bold p-2 ml-6">Email: {order.email}</p>
-                  <p className="text-xl font-bold p-2 ml-6">Delivery Option: {order.deliveryOption}</p>
-                  <button className="p-2 bg-green-500 text-white" onClick={() => setEditIndex(index)}>Edit</button>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-3xl text-center font-bold m-6">No orders found.</p>
-      )}
+                    <p className="text-xl font-bold p-2 ml-6">Name: {order.name}</p>
+                    <p className="text-xl font-bold p-2 ml-6">Address: {order.address}</p>
+                    <p className="text-xl font-bold p-2 ml-6">Email: {order.email}</p>
+                    <p className="text-xl font-bold p-2 ml-6">Delivery Option: {order.deliveryOption}</p>
+                    <button className="p-2 bg-green-500 text-white" onClick={() => setEditIndex(index)}>Edit</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-3xl text-center font-bold m-6">No orders found.</p>
+        )}
     </div>
   );
 }
