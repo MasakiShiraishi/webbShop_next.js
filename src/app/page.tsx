4783a476f5c5617d459f832374d481e5d27e2d6d
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-08-03 12:44:24 +0200

    added link tags at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import SearchTag from "./ui/searchTag";
 import Slider from "./ui/slider";
 
 export default function Home() {
   return (
     <>
     <h1 className="text-4xl text-center pt-12 pb-6">Welcome to webshop</h1>
+    <SearchTag/>
     <Slider/>
     </>
   );
