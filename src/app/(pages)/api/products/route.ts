4133e7485ec98484462a9ab0d95b7938ef97cd7c
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2024-08-05 06:30:34 +0200

    added search moter function in header

--- src/app/(pages)/api/products/route.ts
+++ src/app/(pages)/api/products/route.ts
@@ -8,26 +8,37 @@ export async function GET(req: NextRequest, res: NextResponse) {
   console.log('MongoDB connected');
  
   // Extract the category from the query parameters
+  const search = req.nextUrl.searchParams.get('search');// added for search function
   const category = req.nextUrl.searchParams.get('category');
-  // Log the category to verify it's being received correctly
+
+  console.log('Search Term:', search);
   console.log('Category:', category); 
 
   try {
-    let products;
-    
-    if(category && category !== 'undefined'){
-      // Filter products by category
-      products = await ProductModel.find({category});
-    }else{
-      // Fetch all products if no category is specified
-      products = await ProductModel.find({}); 
-    } 
+    let query = {};
 
-    // -----no change below to be category-----
-    // console.log('Retrieved products:', products);
+    // Construct the query object based on the presence of search and category
+    if (search && search !== 'undefined') {
+      query = {
+        ...query,
+        title: { $regex: search, $options: 'i' }  // Add search criteria for name
+      };
+    }
+    
+    if (category && category !== 'undefined') {
+      query = {
+        ...query,
+        category  // Filter products by category
+      };
+    }
+
+    // Fetch products based on the constructed query
+    const products = await ProductModel.find(query);
+
+    console.log('Retrieved products:', products);
     return NextResponse.json({ products });
   } catch (error) {
     console.error('Error fetching products:', error); 
-    return NextResponse.json({ error: 'Failed to fetch products' });
+    return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
   }
 }
